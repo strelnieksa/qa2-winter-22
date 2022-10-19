06819caeb95536a260e50c303b077a3a02d884f5
strel <strelnieks.a@gmail.com>
2022-10-19 21:16:52 +0300

    Starting Browser and opening home page

--- src/test/java/FirstseleniumTest.java
+++ src/test/java/FirstseleniumTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstseleniumTest {
 
     @Test
     public void openHomePageCheck() {
-
-
+        System.setProperty("webdriver.chrome.driver", "C:\\ideaProject\\chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("http://www.discovercars.com/");
     }
 
 }
