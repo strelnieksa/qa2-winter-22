a61af2bbe38acc76f98b7db5eee084602035c22d
strel <strelnieks.a@gmail.com>
2022-10-19 20:13:47 +0300

    Initial commit

--- src/test/java/StarHomeWork/StarResult.java
+++ src/test/java/StarHomeWork/StarResult.java
@@ -43,36 +43,84 @@ public class StarResult {
 
         rigaSigulda.setDistance(65.05);
         rigaBerlin.setDistance(1249.00);
-        rigaMadrid.setDistance(3532);
+        rigaMadrid.setDistance(6000);
 
         double aRoute = rigaSigulda.getDistance();
         double bRoute = rigaBerlin.getDistance();
         double cRoute = rigaMadrid.getDistance();
 
         double carKm = car.getFuelTankCapacity() / car.getFuelConsumptionOn100km() * 100;
-        double tarinKm = train.getFuelTankCapacity() / train.getFuelConsumptionOn100km() * 100;
+        double trainKm = train.getFuelTankCapacity() / train.getFuelConsumptionOn100km() * 100;
         double plainKm = plane.getFuelTankCapacity() / plane.getFuelConsumptionOn100km() * 100;
 
 
         System.out.println("Car");
-        if ( carKm / bRoute < 0) {
+        if (carKm < aRoute) {
             System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Not enough fuel to take a ride");
         } else {
             System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Enjoy Your journey!");
         }
 
-        if (carKm / bRoute < 0) {
+        if (carKm < bRoute) {
             System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Not enough fuel to take a ride");
         } else {
             System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Enjoy Your journey!");
         }
 
-        if (carKm / cRoute < 0) {
+        if (carKm < cRoute) {
             System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Not enough fuel to take a ride");
         } else {
             System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Enjoy Your journey!");
         }
 
+        System.out.println("Train");
+        if (trainKm < aRoute) {
+            System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Enjoy Your journey!");
+        }
+
+        if (trainKm < bRoute) {
+            System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Enjoy Your journey!");
+        }
+
+        if (trainKm < cRoute) {
+            System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Enjoy Your journey!");
+        }
+
+        System.out.println("Plane");
+        if (plainKm < aRoute) {
+            System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + sigulda.getEndPoint() + " Enjoy Your journey!");
+        }
+
+        if (plainKm < bRoute) {
+            System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + berlin.getEndPoint() + " Enjoy Your journey!");
+        }
+
+        if (plainKm < cRoute) {
+            System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Not enough fuel to take a ride");
+        } else {
+            System.out.println(riga.getStartPoint() + " to " + madrid.getEndPoint() + " Enjoy Your journey!");
+        }
+
+
+
+
+
+        System.out.println(carKm);
+        System.out.println(trainKm);
+        System.out.println(plainKm);
+        System.out.println(aRoute);
+        System.out.println(bRoute);
+        System.out.println(cRoute);
 
     }
 
