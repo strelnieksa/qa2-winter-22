ddbddaa88522806db8159b728dc988bd80169f69
strel <strelnieks.a@gmail.com>
2022-12-16 06:42:01 +0200

    UPDATE

--- src/test/java/myPageObject/BasicFunctions.java
+++ src/test/java/myPageObject/BasicFunctions.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicFunctions {
     private WebDriver browser;
 
-    public BasicFunctions(){
+    public BasicFunctions() {
         System.setProperty("webdriver.chrome.driver", "C:\\ideaProject\\chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
+    }
 
+    public void openUrl(String url) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "http://" + url;
+        }
+        browser.get(url);
     }
 }
--- src/test/java/myPageObject/TicketsTestHomeworkByMyself.java
+++ src/test/java/myPageObject/TicketsTestHomeworkByMyself.java
@@ -3,9 +3,13 @@ package myPageObject;
 import org.junit.jupiter.api.Test;
 
 public class TicketsTestHomeworkByMyself {
-    private BasicFunctions BasicFunctions = new BasicFunctions();
+    private final String URL = "www.qaguru.lv:8089/tickets";
+
+    private BasicFunctions basicFunctions = new BasicFunctions();
     @Test
     public void mySuccesfullRegistrationTest() {
+        basicFunctions.openUrl(URL);
+
 
     }
 }
